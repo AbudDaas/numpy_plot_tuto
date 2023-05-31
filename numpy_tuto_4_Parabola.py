ed9b5ef5ad8ce1727215dae6861c064885c9e7b1
Abud <abdulrazakdaas0@gmail.com>
2023-05-31 14:16:29 +0300

    update

--- numpy_tuto_4_Parabola.py
+++ numpy_tuto_4_Parabola.py
@@ -10,6 +10,7 @@ r**2 = (y-y0)**2+(x-x0)**2
 1 = ((y-y0)/r)**2  + ((x-x0)/r)**2
 
 1 = ((y-y0)/a)**2  + ((x-x0)/b)**2
+y = a(x-h)**2 + k 
 """
 
 x = np.arange(-10, 11, 0.1)
@@ -18,22 +19,25 @@ x0 = 0
 y0 = 0
 # p0 = (x0, y0)
 
-a = 5
+a = 1
 b = 3
 
 # y_pos = np.sqrt(r**2 - (x-x0)**2) + y0 
 #y_pos = y0 + np.sqrt(b**2(a**2-(x-x0)**2) )
-y_pos = y0+ np.sqrt((1-((x-x0)/b)**2)) 
+y_pos =np.sqrt(4*a*x)
 y_neg = - y_pos
-
+x_pos = np.sqrt(-4*a*x)
+x_neg = -x_pos
 plt.xlabel("X-Axis")
 plt.ylabel("Y-Axis")
 
-plt.xlim(-5, 5)
-plt.ylim(-5, 5)
+plt.xlim(-20, 20)
+plt.ylim(-20, 20)
 
 plt.plot(x, y_pos, color='g')
 plt.plot(x, y_neg, color='g')
+plt.plot(x, x_pos, color='g')
+plt.plot(x, x_neg, color='g')
 
 plt.scatter(x0, y0)
 
